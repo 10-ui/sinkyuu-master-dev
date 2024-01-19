b520f5f97b7bcc2281fa397f1cace431ca52676e
Sentaro <ui-10@outlook.jp>
2024-01-19 15:48:45 +0900

    fix li and ul

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function Header() {
   const [ham, setHam] = useState(false);
@@ -17,54 +17,85 @@ function Header() {
           </a>
         </h1>
         <div className="menu-btn" onClick={toggleHammenu}>
-          <div className={`${ham ? "line-active" : ""} line`}></div>
+          <div
+            className={`${
+              ham ? 'line-active' : ''
+            } line`}></div>
         </div>
       </div>
 
-      <nav className={`${ham ? "active" : ""} menu`}>
+      <nav className={`${ham ? 'active' : ''} menu`}>
         <ul>
-          <h2>solotori公式サイト</h2>
+          <li>
+            <h2>solotori公式サイト</h2>
+          </li>
           <li>
             <p className="papa">見つける</p>
-            <li className="footer-list_item">
-              <a href="https://solotori.jp/concept/">コンセプトを見つける</a>
-            </li>
-            <li className="footer-list_item">
-              <a href="https://solotori.jp/event/">イベントを見つける</a>
-            </li>
-            <li className="footer-list_item">
-              <a href="https://solotori.jp/area/">都道府県から見つける</a>
-            </li>
+            <ul>
+              <li className="footer-list_item">
+                <a href="https://solotori.jp/concept/">
+                  コンセプトを見つける
+                </a>
+              </li>
+              <li className="footer-list_item">
+                <a href="https://solotori.jp/event/">
+                  イベントを見つける
+                </a>
+              </li>
+              <li className="footer-list_item">
+                <a href="https://solotori.jp/area/">
+                  都道府県から見つける
+                </a>
+              </li>
+            </ul>
           </li>
           <li>
             <p className="papa">ガイド</p>
-            <li className="footer-list_item">
-              <a href="https://hone-corp.jp/solotori-howtouse">ご利用ガイド</a>
-            </li>
-            <li className="footer-list_item">
-              <a href="https://solotori.jp/contact/">お問い合わせ</a>
-            </li>
-            <li className="footer-list_item">
-              <a href="https://hone-corp.jp/solotori/lp-local">掲載案内</a>
-            </li>
+            <ul>
+              <li className="footer-list_item">
+                <a href="https://hone-corp.jp/solotori-howtouse">
+                  ご利用ガイド
+                </a>
+              </li>
+              <li className="footer-list_item">
+                <a href="https://solotori.jp/contact/">
+                  お問い合わせ
+                </a>
+              </li>
+              <li className="footer-list_item">
+                <a href="https://hone-corp.jp/solotori/lp-local">
+                  掲載案内
+                </a>
+              </li>
+            </ul>
           </li>
           <li>
             <p className="papa">企業情報</p>
-            <li className="footer-list_item">
-              <a href="https://hone-corp.jp/">運営会社</a>
-            </li>
-            <li className="footer-list_item">
-              <a href="https://hone-corp.jp/solotori-terms">利用契約</a>
-            </li>
-            <li className="footer-list_item">
-              <a href="https://hone-corp.jp/solotori-privacy">
-                プライバシーポリシー
-              </a>
-            </li>
+            <ul>
+              <li className="footer-list_item">
+                <a href="https://hone-corp.jp/">運営会社</a>
+              </li>
+              <li className="footer-list_item">
+                <a href="https://hone-corp.jp/solotori-terms">
+                  利用契約
+                </a>
+              </li>
+              <li className="footer-list_item">
+                <a href="https://hone-corp.jp/solotori-privacy">
+                  プライバシーポリシー
+                </a>
+              </li>
+            </ul>
           </li>
         </ul>
         <div className="flexes">
-          <a href="https://www.instagram.com/solotori_official/?hl=ja"><img src="./public/insuta.webp" alt="Icon" className="zizi" /></a>
+          <a href="https://www.instagram.com/solotori_official/?hl=ja">
+            <img
+              src="./public/insuta.webp"
+              alt="Icon"
+              className="zizi"
+            />
+          </a>
           <button className="botan">お問い合わせ</button>
         </div>
       </nav>
